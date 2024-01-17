bee9121d705b4c7625add92a6ec9776569ab17ed
mohammed.benslimane <benshamou@gmail.com>
2024-01-17 16:52:30 +0100

    admin url

--- pumalabs/urls.py
+++ pumalabs/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("puma_labs/admin/", admin.site.urls),
     path("", include('account.urls')),
     path("", include('report.urls')),
 ]
