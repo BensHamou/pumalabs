2521fc6ff1798b9a181ed75c815512e44d68cbdd
mohammed.benslimane <benshamou@gmail.com>
2024-01-18 09:29:16 +0100

    new API for app

--- account/authentication.py
+++ account/authentication.py
@@ -14,7 +14,7 @@ class ApiBackend(BaseBackend):
         
         auth = HTTPBasicAuth(username, password)
 
-        response = requests.post('https://api.ldap.groupe-hasnaoui.com/pumaprd/auth', auth=auth)
+        response = requests.post('https://api.ldap.groupe-hasnaoui.com/pumalabs/auth', auth=auth)
 
         if response.status_code == 200 and response.json().get('authenticated'):
             user = User.objects.get(username = response.json().get('userinfo')['ad2000'])
