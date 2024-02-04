d676f397ea0afd9f4a03885b650acf8da570aa72
mohammed.benslimane <benshamou@gmail.com>
2024-02-04 09:13:26 +0100

    optimize authen

--- account/authentication.py
+++ account/authentication.py
@@ -10,34 +10,27 @@ from django.db.models import Q
 class ApiBackend(BaseBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         try:
+            user = User.objects.get(Q(username__iexact=username) | Q(email__iexact=username))
             if username in ['admin', 'admin@admin.com']:
-                user = User.objects.get(Q(username=username) | Q(email=username))
                 if check_password(password, user.password):
                     return user
                 else:
-                    messages.error(request, "Mot de passe incorrect")
+                    messages.error(request, "Mot de passe incorrect.")
                     return None
             
-            if '@' not in username:
-                user = User.objects.get(username = username)
-                email = user.email
-            else:
-                email = username
-                
-            auth = HTTPBasicAuth(email, password)
+            auth = HTTPBasicAuth(user.email, password)
 
             response = requests.post('https://api.ldap.groupe-hasnaoui.com/pumalabs/auth', auth=auth)
 
             if not response.status_code == 200:
-                messages.error(request, "Problème avec la connexion au serveur")
+                messages.error(request, "Problème avec la connexion au serveur.")
             else:
                 if not response.json().get('authenticated'):
-                    messages.error(request, "Mot de passe incorrect")
+                    messages.error(request, "Mot de passe incorrect.")
                 else:
-                    user = User.objects.get(username = response.json().get('userinfo')['ad2000'])
                     return user
         except User.DoesNotExist:
-            messages.error(request, "Utilisateur pas trouvé")
+            messages.error(request, "Utilisateur pas trouvé.")
         return None
     
     def get_user(self, user_id):
