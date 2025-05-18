70f9ed1492c27cd77775579d289162fe33563604
Benslimane Mohammed <benshamou@gmail.com>
2025-05-18 16:10:16 +0100

    change production pwd

--- pumalabs/settings.py
+++ pumalabs/settings.py
@@ -149,6 +149,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'pumalabs001@gmail.com'
-EMAIL_HOST_PASSWORD = 'zzynmnuizkbjtpoo'
+EMAIL_HOST_PASSWORD = 'rzkrttpwusfjkcqb'
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'pumalabs001@gmail.com'
